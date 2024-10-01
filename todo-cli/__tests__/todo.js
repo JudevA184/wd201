d1957d1386192c5331d7a5eb68cc70f4712d947f
Judev <judev@DRAGO>
2024-10-01 23:18:12 +0530

    Add pre commit hook to run tests

--- todo-cli/__tests__/todo.js
+++ todo-cli/__tests__/todo.js
@@ -1,27 +1,54 @@
+/* eslint-disable no-undef */
 const todoList=require('../todo')
-const{all, markAsComplete, add}=todoList()
+const{all, markAsComplete, add, overdue, dueToday, dueLater}=todoList()
+
+
+const formattedDate = d => {
+  return d.toISOString().split("T")[0]
+}
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
 
 describe("todo test suite",()=>{
-    beforeAll(()=>{
-        add({
-        title:"test todo",
-        completed:false,
-        duedate:"today"
-        })
-    })
-    test("empty check",()=>{
+    test("new todo",()=>{
         const todoCount=all.length
         add({
-        title:"test todo",
-        completed:false,
-        duedate:"today"
+            title:"add new todo",
+            completed:false,
+            dueDate:today
         })
-        expect(all.length).toBe(todoCount+1);
+        expect(all.length).toBe(todoCount+1)
     })
     test("check for completed",()=>{
         expect(all[0].completed).toBe(false)
         markAsComplete(0)
         expect(all[0].completed).toBe(true)
     })
-    
+    test("check for overdue",()=>{
+        const overd={title:"an od task",completed:false,dueDate:yesterday}
+        const overdlen=overdue().length;
+        add(overd)
+        const newoverdlen=overdue().length;
+        expect(newoverdlen).toBe(overdlen+1)
+    })
+    test("check for duetoday",()=>{
+        const deuTOD={title:"a task for today",completed:false,dueDate:today}
+        const deuTODlen=dueToday().length;
+        add(deuTOD)
+        const newdeuTODlen=dueToday().length;
+        expect(newdeuTODlen).toBe(deuTODlen+1)
+    })
+    test("check for dueLater",()=>{
+        const dueL={title:"a task for later",completed:false,dueDate:tomorrow}
+        const dueLlen=dueLater().length;
+        add(dueL)
+        const newdueLlen=dueLater().length;
+        expect(newdueLlen).toBe(dueLlen+1)
+    })
 })
--- todo-cli/todo.js
+++ todo-cli/todo.js
@@ -1,5 +1,16 @@
 const todoList = () => {
-  all = []
+  const formattedDate = d => {
+  return d.toISOString().split("T")[0]
+}
+var dateToday = new Date()
+const today = formattedDate(dateToday)
+const yesterday = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() - 1))
+)
+const tomorrow = formattedDate(
+  new Date(new Date().setDate(dateToday.getDate() + 1))
+)
+  let all = []
   const add = (todoItem) => {
     all.push(todoItem)
   }
@@ -10,7 +21,7 @@ const todoList = () => {
   const overdue = () => {
     // Write the date check condition here and return the array
     // of overdue items accordingly.
-    arr=[]
+    let arr=[]
     all.forEach(element => {
         if(element.dueDate===yesterday){
             arr.push(element)
@@ -22,7 +33,7 @@ const todoList = () => {
   const dueToday = () => {
     // Write the date check condition here and return the array
     // of todo items that are due today accordingly.
-    arr=[]
+    let arr=[]
     all.forEach(element => {
         if(element.dueDate===today){
             arr.push(element)
@@ -34,7 +45,7 @@ const todoList = () => {
   const dueLater = () => {
     // Write the date check condition here and return the array
     // of todo items that are due later accordingly.
-    arr=[]
+    let arr=[]
     all.forEach(element => {
         if(element.dueDate===tomorrow){
             arr.push(element)
@@ -46,7 +57,7 @@ const todoList = () => {
   const toDisplayableList = (list) => {
     // Format the To-Do list here, and return the output string
     // as per the format given above.
-    arr=[]
+    let arr=[]
     list.forEach(element=>{
         if(element.dueDate===yesterday || element.dueDate===tomorrow){
             if(element.completed===true){
